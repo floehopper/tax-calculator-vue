3ed3b3392b63ea580f8633e005ce5bc831200307
James Mead <james@floehopper.org>
2018-04-21 17:01:12 +0100

    Add reactive bar graph using chart.js

---
+++ src/components/NationalInsuranceBarGraph.vue
@@ -0,0 +1,22 @@
+<script>
+import {Bar, mixins} from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveProp],
+  mounted () {
+    this.renderChart(this.chartData, {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [{
+          stacked: true
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    })
+  }
+}
+</script>
--- src/components/TaxCalculator.vue
+++ src/components/TaxCalculator.vue
@@ -1,37 +1,61 @@
 <template>
   <div>
     <label>
-      Salary: <input type="number" v-model="salary"/>
+      Salary: £<input type="number" v-model="salary"/>
     </label>
-    <p>Employee contributions: {{ employeeContributions }}</p>
-    <p>Employer contributions: {{ employerContributions }}</p>
+    <p>Employee contributions: £{{ employeeContributions }}</p>
+    <p>Employer contributions: £{{ employerContributions }}</p>
+
+    <national-insurance-bar-graph :chart-data="chartData"></national-insurance-bar-graph>
   </div>
 </template>
 
 <script>
+import NationalInsuranceBarGraph from '@/components/NationalInsuranceBarGraph'
 import EmployeeNationalInsurance from '@/components/EmployeeNationalInsurance'
 import EmployerNationalInsurance from '@/components/EmployerNationalInsurance'
-import CurrencyFormatter from 'currency-formatter'
 
 export default {
   name: 'TaxCalculator',
+  components: {
+    NationalInsuranceBarGraph
+  },
   data () {
     return {
-      salary: 0
+      employerCalculator: EmployerNationalInsurance.for2018to2019(),
+      employeeCalculator: EmployeeNationalInsurance.for2018to2019(),
+      salary: 20000
     }
   },
   computed: {
-    employeeContributions () {
-      const amount = EmployeeNationalInsurance.for2018to2019().taxFor(this.salary)
-      return CurrencyFormatter.format(amount, { code: 'GBP' })
-    },
-
     employerContributions () {
-      const amount = EmployerNationalInsurance.for2018to2019().taxFor(this.salary)
-      return CurrencyFormatter.format(amount, { code: 'GBP' })
+      return this.roundToNearestPenny(this.employerCalculator.taxFor(this.salary))
+    },
+    employeeContributions () {
+      return this.roundToNearestPenny(this.employeeCalculator.taxFor(this.salary))
+    },
+    chartData () {
+      return {
+        labels: ['NI Contributions'],
+        datasets: [
+          {
+            label: 'Employer',
+            backgroundColor: 'rgb(54, 162, 235)',
+            data: [this.roundToNearestPenny(this.employerCalculator.taxFor(this.salary))]
+          },
+          {
+            label: 'Employee',
+            backgroundColor: 'rgb(255, 99, 132)',
+            data: [this.roundToNearestPenny(this.employeeCalculator.taxFor(this.salary))]
+          }
+        ]
+      }
     }
   },
   methods: {
+    roundToNearestPenny (amount) {
+      return Number.parseFloat(amount).toFixed(2)
+    }
   }
 }
 </script>
